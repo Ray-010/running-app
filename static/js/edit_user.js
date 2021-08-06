794955a87a378ab68b70098d841f4326a510c6e3
HaseU-git <hasegawayuta@MacBook-Pro-3.local>
2021-08-06 14:31:16 +0900

    Fix:
    fix test.js

--- static/js/edit_user.js
+++ static/js/edit_user.js
@@ -1,16 +1,26 @@
-window.onload = function() {
-	test()
-	document.getElementById('user_name').textContent = user_name;
-	document.getElementById('user_weight').textContent = body_weight;
-	document.getElementById('user_goal_weight').textContent = user_name;
-	document.getElementById('user_point').textContent = user_point;
+// window.onload = function() {
+// 	test()
+// 	document.getElementById('user_name').textContent = user_name;
+// 	document.getElementById('user_weight').textContent = body_weight;
+// 	document.getElementById('user_goal_weight').textContent = user_name;
+// 	document.getElementById('user_point').textContent = user_point;
+// }
+
+window.onload = show_user_info();
+
+function show_user_info()
+{
+		document.getElementById('user_name').textContent = sessionStorage.getItem("actual_name");
+		document.getElementById('user_weight').textContent = sessionStorage.getItem("body_weight");
+		document.getElementById('user_goal_weight').textContent = sessionStorage.getItem("user_name");
+		document.getElementById('user_point').textContent = sessionStorage.getItem("user_point");
 }
 
 // 編集
 function save_info() {
-  user_name = document.getElementById('new_user_name')
-  // body_weight = document.getElementById('new_body_weight')
-  test()
+  sessionStorage.setItem("user_name", document.getElementById('new_user_name'));
+  sessionStorage.setItem("body_weight", document.getElementById('new_body_weight'));
+  sessionStorage.setItem("target_weight", document.getElementById("new_goal_weight"));
 }
 
 // 編集画面を閉じる
